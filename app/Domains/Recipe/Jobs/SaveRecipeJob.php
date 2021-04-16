f89e48ca8be129da9043310b9503a83b602a304e
Imali Susan <susanimali52@gmail.com>
2021-04-16 01:40:48 -0700

    Add save recipe feature

--- app/Domains/Recipe/Jobs/SaveRecipeJob.php
+++ app/Domains/Recipe/Jobs/SaveRecipeJob.php
@@ -2,7 +2,9 @@
 
 namespace App\Domains\Recipe\Jobs;
 
+use App\Models\User;
 use Lucid\Units\Job;
+use App\Data\Models\Recipe;
 
 class SaveRecipeJob extends Job
 {
@@ -11,7 +13,12 @@ class SaveRecipeJob extends Job
      *
      * @return void
      */
-    public function __construct()
+    public function __construct(
+        private string $title,
+        private string $ingredients,
+        private string $instructions,
+        private string $price,
+        private User $user)
     {
         //
     }
@@ -21,8 +28,17 @@ class SaveRecipeJob extends Job
      *
      * @return void
      */
-    public function handle()
+    public function handle(): Recipe
     {
-        //
+        $attributes = [
+            'title' => $this->title,
+            'ingredients' => $this->ingredients,
+            'instructions' => $this->instructions,
+            'price' => $this->price,
+            'user_id' => $this->user->getKey(),
+        ];
+
+        return tap(new Recipe($attributes))->save();
+
     }
 }
--- app/Services/Kitchen/Features/AddRecipeFeature.php
+++ app/Services/Kitchen/Features/AddRecipeFeature.php
@@ -4,6 +4,8 @@ namespace App\Services\Kitchen\Features;
 
 use Lucid\Units\Feature;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\Domains\Recipe\Jobs\SaveRecipeJob;
 use App\Domains\Recipe\Requests\AddRecipe;
 use App\Domains\Recipe\Jobs\ParseIngredientsJob;
 use App\Domains\Recipe\Jobs\CalculateIngredientsTotalJob;
@@ -16,5 +18,15 @@ class AddRecipeFeature extends Feature
         $price = $this->run(CalculateRecipePriceOperation::class, [
             'ingredients' => $request->input('ingredients'),
         ]);
+
+        $this->run(SaveRecipeJob::class, [
+            'title' => $request->input('title'),
+            'ingredients' => $request->input('ingredients'),
+            'instructions' => $request->input('instructions'),
+            'price' => $price,
+            'user' => Auth::user(),
+        ]);
+
+        return $this->run(RedirectBackJob::class);
     }
 }
